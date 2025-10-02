7120c3db5c9bbfd7c0f08e447e0dffaf8e3e759a
Amit-Biswas-WD <155246211+Amit-Biswas-WD@users.noreply.github.com>
2025-10-02 12:06:26 +0600

    Admin er Dashboard Design Done

--- src/page/private/admin/components/dashboard/components/WaitListTable.jsx
+++ src/page/private/admin/components/dashboard/components/WaitListTable.jsx
@@ -2,10 +2,22 @@ import { AllTableResponsiveStyle } from "../../../../../../components/AllTableRe
 
 export const WaitListTable = () => {
   const data = [
-    { date: "Jul 5, 2025", name: "Bessie Cooper", email: "bill.sanders@example.com" },
+    {
+      date: "Jul 5, 2025",
+      name: "Bessie Cooper",
+      email: "bill.sanders@example.com",
+    },
     { date: "Jul 6, 2025", name: "John Doe", email: "john.doe@example.com" },
-    { date: "Jul 7, 2025", name: "Jane Smith", email: "jane.smith@example.com" },
-    { date: "Jul 8, 2025", name: "Michael Brown", email: "michael.brown@example.com" },
+    {
+      date: "Jul 7, 2025",
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+    },
+    {
+      date: "Jul 8, 2025",
+      name: "Michael Brown",
+      email: "michael.brown@example.com",
+    },
   ];
 
   return (
@@ -21,7 +33,10 @@ export const WaitListTable = () => {
           </thead>
           <tbody className="text-black text-base font-normal">
             {data.map((row, index) => (
-              <tr key={index} className={index % 2 === 0 ? "bg-yellow-50" : "bg-white"}>
+              <tr
+                key={index}
+                className={index % 2 === 0 ? "bg-yellow-50" : "bg-white"}
+              >
                 <td className="px-7 py-3 w-1/3">{row.date}</td>
                 <td className="px-7 py-3 w-1/3">{row.name}</td>
                 <td className="px-7 py-3 w-1/3">{row.email}</td>
@@ -31,7 +46,19 @@ export const WaitListTable = () => {
         </table>
       </div>
 
-      <AllTableResponsiveStyle/>
+      <AllTableResponsiveStyle />
+
+      <div className="flex items-center text-gray-600 justify-between mt-8 text-base font-poppins font-normal">
+        <p className="font-inter ">Showing 1 to 4 of 4 results</p>
+        <div className="flex gap-4 sm:gap-5 md:gap-6 lg:gap-7">
+          <button className="border border-gray-600 rounded-xl px-5 py-2">
+            Previous
+          </button>
+          <button className="border border-gray-600 rounded-xl px-5 py-2">
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
