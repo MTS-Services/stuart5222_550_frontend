05dd54a9f56d77fc85effe7171a93f76bfdbcfa5
Amit-Biswas-WD <155246211+Amit-Biswas-WD@users.noreply.github.com>
2025-10-02 10:59:17 +0600

    Branch Amit

--- src/page/private/admin/components/dashboard/AdminDashboard.jsx
+++ src/page/private/admin/components/dashboard/AdminDashboard.jsx
@@ -1,9 +1,22 @@
+import { AllCard } from "./AllCard";
+import { DateDropDown } from "./DateDropDown";
+
 export const AdminDashboard = () => {
   return (
-    <div className="text-black">
+    <div className="text-black p-8">
       <div className="">
-        <h2 className="text-2xl font-semibold">Dashboard Overview</h2>
+        <h2 className="text-2xl text-[#002244] font-semibold font-poppins">
+          Dashboard Overview
+        </h2>
+        <p className="justify-start text-[#464646] text-base font-normal font-lato leading-normal mb-3 mt-1">
+          Monitor your user performance
+        </p>
+        <h3 className="justify-center text-[#111827] text-base font-normal font-['Poppins'] leading-normal">
+          Last 30 days overview
+        </h3>
+        <DateDropDown/>
       </div>
+      <AllCard/>
     </div>
   );
 };
