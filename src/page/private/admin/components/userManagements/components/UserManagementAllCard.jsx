585aaefcc0e3e5ff8f4bfe4031715ce7fabaa91c
Amit-Biswas-WD <155246211+Amit-Biswas-WD@users.noreply.github.com>
2025-10-02 13:22:32 +0600

    Admin UserManagement table and modal responsive done

--- src/page/private/admin/components/userManagements/components/UserManagementAllCard.jsx
+++ src/page/private/admin/components/userManagements/components/UserManagementAllCard.jsx
@@ -31,16 +31,16 @@ export const UserManagementAllCard = () => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-5 md:gap-6 lg:gap-8 mt-2">
       {cardData.map((data) => (
-        <div className="rounded-lg border border-white/100 bg-white p-5 col-span-1">
+        <div className="rounded-lg border border-white/100 bg-white p-2 sm:p-4 md:p-5 col-span-1">
           <div className="flex-col justify-center items-start gap-2">
             <div className="justify-start text-White-800 text-xs font-normal font-lato leading-none">
               {data.title}
             </div>
             <div className="flex items-center justify-between my-1">
-              <div className="justify-start text-White-800 text-2xl font-semibold font-poppins leading-9">
+              <div className="justify-start text-White-800 text-lg sm:text-xl md:text-2xl font-semibold font-poppins leading-9">
                 {data.price}
               </div>
-              <div className="bg-green-50 p-3 rounded-lg">
+              <div className="bg-green-50 p-2 sm:p-2.5 md:p-3 rounded-lg">
                 <FaUsers className="w-5 h-5 text-gray-800" />
               </div>
             </div>
