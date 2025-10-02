36fbe71d446336b22a4d67a29768460a78921fa4
Amit-Biswas-WD <155246211+Amit-Biswas-WD@users.noreply.github.com>
2025-10-02 18:08:50 +0600

    icon

--- src/page/private/admin/AdminDashboardLayout.jsx
+++ src/page/private/admin/AdminDashboardLayout.jsx
@@ -17,7 +17,9 @@ export const AdminDashboardLayout = () => {
             <Link to={`/admin/notification`}>
               <FaRegBell />
             </Link>
-            <FaRegUser />
+            <div className=" bg-green-50 rounded-full p-2.5">
+              <FaRegUser className="w-4 h-4" />
+            </div>
             <div className="font-medium text-black/90">
               <h2 className="text-base font-semibold mb-1">John Davis</h2>
               <p className="text-sm">Administrator</p>
