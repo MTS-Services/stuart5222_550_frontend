664113ad5b5ae5b4aa51f169b1c9c9f7e0447ff4
Nayem Islam <gangsn490@gmail.com>
2025-10-16 15:57:15 +0600

    feat: Refactor user management by replacing user slices and fetches with admin-specific implementations

--- src/features/public/user/userFetch.js
+++ src/features/public/user/userFetch.js
@@ -1,15 +1,15 @@
 // src/features/user/userFetch.js
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { endpoints } from '../../config/api/httpEndpoint';
-import { STORAGE } from '../../config/storage/auth/authStorage';
-import { POST } from '../../config/api/httpMethods';
+import { endpoints } from '../../../config/api/httpEndpoint';
+import { STORAGE } from '../../../config/storage/auth/authStorage';
+import { POST } from '../../../config/api/httpMethods';
 
 // ========== Login ==========
 export const requestWaitlist = createAsyncThunk(
   'user/waitlist',
   async ({ name, email }, { rejectWithValue }) => {
     try {
-      const res = await POST(endpoints.user.WAITLIST, { name, email });
+      const res = await POST(endpoints.user.REGISTER, { name, email });
 
       console.log('Waitlist Response:', res.message);
 
